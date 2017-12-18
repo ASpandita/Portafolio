4428dbff3e9ee880e52ec4a7b4b424ad804263f6
Adrian Simancas <adriansimancas19@gmail.com>
2017-12-18 12:33:17 -0300

    Add files via upload

---
+++ telollevoweb/login.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registro</title>
+</head>
+<body>
+<?php
+
+	include("conexionBD.php");
+
+	$nombre = $_POST["nombreA"];
+	$pass = $_POST["passA"];
+	$resp = "hola";
+	$query = "SELECT * FROM admin WHERE nombre_adm = '$nombre' and pass_adm = '$pass'";
+	
+
+	$result = pg_query($dbcon,$query);		
+	$result_array=pg_fetch_array($result);
+	if (pg_num_rows($result)>0) {
+		$resp = json_encode(array("login"=>true));
+
+		
+	}else{
+		$resp = json_encode(array("login"=>false));
+	}
+	
+	
+
+
+
+?>
+<script type="text/javascript">
+	var log = <?php echo "'$resp'"; ?>;
+	
+	var json = JSON.parse(log);
+	if (json.login==true) {
+		window.location = "nitro/index.html";
+	}else{
+		alert("nombre o contraseña incorrecta")
+		window.location = "administrador.html";
+	}
+
+
+
+</script>
+
+</body>
+</html>
